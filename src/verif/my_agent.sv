f9346713f84a9f45378cc1db6cfe6eb0fb76591d
MushroomZQ <MushroomZQ@users.noreply.github.com>
2017-09-29 14:36:42 +0800

    update agent/env for the new components

--- src/verif/my_agent.sv
+++ src/verif/my_agent.sv
@@ -2,21 +2,33 @@ class my_agent extends uvm_agent;
 
     my_sequencer my_sqr;
     my_driver my_drv;
+    my_monitor my_mo;
+    my_predictor my_pred;
+
+    uvm_tlm_analysis_fifo#(my_transaction) agt_mdl_fifo;
+    uvm_analysis_port#(my_transaction) mon_scbd_ap;
 
     `uvm_component_utils(my_agent)
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
+        agt_mdl_fifo = new("agt_mdl_fifo", this);
     endfunction
 
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
         my_sqr = my_sequencer::type_id::create("my_sqr", this);
         my_drv = my_driver::type_id::create("my_drv", this);
+        my_mo = my_monitor::type_id::create("my_mo", this);
+        my_pred = my_predictor::type_id::create("my_pred", this);
     endfunction
 
     function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
         my_drv.seq_item_port.connect(my_sqr.seq_item_export);
+        my_mo.mon_pred_ap.connect(agt_mdl_fifo.blocking_get_export);
+        my_pred.mon_pred_port.connect(agt_mdl_fifo.analysis_export);
+        mon_scbd_ap = my_mo.mon_scbd_ap;
     endfunction
 
 endclass: my_agent
--- src/verif/my_env.sv
+++ src/verif/my_env.sv
@@ -1,6 +1,8 @@
 class my_env extends uvm_env;
 
     my_agent my_agt;
+    my_predictor my_pred;
+    my_scoreboard my_scbd;
 
     `uvm_component_utils(my_env)
 
@@ -12,7 +14,14 @@ class my_env extends uvm_env;
         super.build_phase(phase);
 
         my_agt = my_agent::type_id::create("my_agt", this);
+        my_pred = my_predictor::type_id::create("my_pred", this);
+        my_scbd = my_scoreboard::type_id::create("my_scbd", this);
+    endfunction
 
+    function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+        my_pred.pred_scbd_ap.connect(my_scbd.pred_scbd_port);
+        my_agt.mon_scbd_ap.connect(my_scbd.mon_scbd_port);
     endfunction
 
 endclass: my_env
