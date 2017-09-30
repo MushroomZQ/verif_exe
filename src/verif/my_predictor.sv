167cf4c0145ffd3b5ccd0c17f734564ffa816e38
MushroomZQ <MushroomZQ@users.noreply.github.com>
2017-09-30 11:26:23 +0800

    update all env to fix port connection issue, now test can finish smoothly

--- src/verif/my_predictor.sv
+++ src/verif/my_predictor.sv
@@ -1,13 +1,18 @@
 class my_predictor extends uvm_component;
 
-    uvm_blocking_get_port#(my_transaction) mon_pred_port;
+    my_transaction input_trans;
+    my_transaction output_trans;
+
+    //uvm_blocking_get_port#(my_transaction) mon_pred_port;
+    `uvm_analysis_imp_decl(_mon_pred)
+    uvm_analysis_imp_mon_pred#(my_transaction, my_predictor) mon_pred_imp;
     uvm_analysis_port#(my_transaction) pred_scbd_ap;
 
     `uvm_component_utils(my_predictor)
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
-        mon_pred_port = new("mon_pred_port", this);
+        mon_pred_imp = new("mon_pred_imp", this);
         pred_scbd_ap = new("pred_scbd_ap", this);
     endfunction
 
@@ -15,15 +20,23 @@ class my_predictor extends uvm_component;
         super.build_phase(phase);
     endfunction
 
-    task run_phase(uvm_phase phase);
+    function void write_mon_pred(my_transaction _trans);
+        do_prediction(_trans);
+    endfunction
+
+    function void do_prediction(my_transaction _trans);
+        output_trans = new();
+        output_trans = _trans;
+        pred_scbd_ap.write(output_trans);
+    endfunction
+
+    /*task run_phase(uvm_phase phase);
         fork
             do_prediction();
         join
     endtask
 
     virtual task do_prediction();
-        my_transaction input_trans;
-        my_transaction output_trans;
         input_trans = new();
         output_trans = new();
     
@@ -31,5 +44,5 @@ class my_predictor extends uvm_component;
         output_trans = input_trans;
 
         pred_scbd_ap.write(output_trans);
-    endtask
+    endtask*/
 endclass
--- src/verif/my_scoreboard.sv
+++ src/verif/my_scoreboard.sv
@@ -6,15 +6,26 @@ class my_scoreboard extends uvm_scoreboard;
 
     int match;
 
-    uvm_blocking_get_port#(my_transaction) mon_scbd_port;
-    uvm_blocking_get_port#(my_transaction) pred_scbd_port;
+    //my_monitor(output_monitor)->my_scoreboard
+    `uvm_analysis_imp_decl (_mon_scbd)
+    uvm_analysis_imp_mon_scbd#(my_transaction, my_scoreboard) mon_scbd_imp;
+
+   //my_predictor->my_scoreboard
+   `uvm_analysis_imp_decl (_pred_scbd)
+   uvm_analysis_imp_pred_scbd#(my_transaction, my_scoreboard) pred_scbd_imp;
+
+    //uvm_blocking_get_port#(my_transaction) mon_scbd_port;
+    //uvm_blocking_get_port#(my_transaction) pred_scbd_port;
 
     `uvm_component_utils(my_scoreboard)
 
     function new(string name, uvm_component parent);
         super.new(name, parent);
-        mon_scbd_port = new("mon_scbd_port", this);
-        pred_scbd_port = new("pred_scbd_port", this);
+        //mon_scbd_port = new("mon_scbd_port", this);
+        //pred_scbd_port = new("pred_scbd_port", this);
+
+        mon_scbd_imp = new("mon_scbd_imp", this);
+        pred_scbd_imp = new("pred_scbd_imp", this);
         match = 0;
     endfunction
 
@@ -22,7 +33,29 @@ class my_scoreboard extends uvm_scoreboard;
         super.build_phase(phase);
     endfunction
 
-    task run_phase(uvm_phase phase);
+    function void write_pred_scbd(my_transaction _trans);
+        pred_trans = new();
+        pred_trans = _trans;
+        pred_queue.push_back(pred_trans);
+    endfunction
+
+    function void write_mon_scbd(my_transaction _trans);
+        real_trans = new();
+        ref_trans = new();
+        real_trans = _trans;
+        ref_trans = pred_queue.pop_front();
+        if(real_trans.compare(ref_trans)) begin
+            match = 1;
+            `uvm_info(get_type_name(), "compare pass", UVM_LOW);
+        end
+        else begin
+            match = 0;
+            `uvm_error(get_type_name(), $psprintf("\nreal_trans is \n%s",real_trans.sprint()));
+            `uvm_error(get_type_name(), $psprintf("\nref_trans is \n%s",ref_trans.sprint()));
+        end
+    endfunction
+
+    /*task run_phase(uvm_phase phase);
         fork
             get_pred_trans();
             compare_trans();
@@ -31,14 +64,16 @@ class my_scoreboard extends uvm_scoreboard;
 
     task get_pred_trans();
         forever begin
-            pred_scbd_port.get(pred_trans);
+            //pred_scbd_port.get(pred_trans);
+            pred_scbd_imp.get(pred_trans);
             pred_queue.push_back(pred_trans);
         end
     endtask
 
     task compare_trans();
         forever begin
-            mon_scbd_port.get(real_trans);
+            //mon_scbd_port.get(real_trans);
+            mon_scbd_imp.get(real_trans);
             if(pred_queue.size()>0) begin
                 ref_trans = pred_queue.pop_front();
                 if(ref_trans.compare(real_trans)) begin
@@ -57,6 +92,6 @@ class my_scoreboard extends uvm_scoreboard;
                 #100;
             end
         end
-    endtask
+    endtask*/
 
 endclass
